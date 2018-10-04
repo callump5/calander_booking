ac49a988b226ee5eda3c7c13b4ab7bde2197de39
Callum <clpullinger@gmail.com>
2018-10-04 20:42:32 +0100

    ddd

--- bookings/models.py
+++ bookings/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from datetime import time
 
 # Create your models here.
 
 
 class Booking(models.Model):
+
+
+    TIME_CHOICES = (
+        (time(11, 00, 00), u'11 AM'),
+        (time(12, 00, 00), u'12 PM'),
+        (time(13, 00, 00), u'1 PM'),
+        (time(14, 00, 00), u'2 PM'),
+        (time(15, 00, 00), u'3 PM'),
+        (time(16, 00, 00), u'4 PM'),
+        (time(17, 00, 00), u'5 PM'),
+    )
+
+
     day = models.DateField(u'Day of booking', help_text='Day of booking')
-    start_time = models.TimeField(u'Booking slot', help_text='Booking slot')
-    end_time = models.TimeField(u'Finish time', help_text='Finish Time')
+
+
+    start_time = models.TimeField(u'Booking slot', help_text='Booking slot', choices=TIME_CHOICES, max_length=200)
+    end_time = models.TimeField(u'Finish time', help_text='Finish Time', choices=TIME_CHOICES, max_length=200)
     notes = models.TextField(u'Notes', help_text='Notes', blank=True, null=True)
 
 
@@ -21,8 +37,10 @@ class Booking(models.Model):
 
     def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
         overlap = False
-        if new_start == fixed_end or new_end == fixed_end: # Edge case
+        if new_start == fixed_end: # Edge case
             overlap = False
+        elif new_start == fixed_start or new_end == fixed_end:
+            overlap = True
         elif(new_start >= fixed_start and new_start <= fixed_end) or (new_end >= fixed_start and new_end <= fixed_end): #Inner limits
             overlap = True
         elif new_start <= fixed_start and new_end >= fixed_end: # Outer limits
@@ -39,4 +57,5 @@ class Booking(models.Model):
         if events.exists():
             for event in events:
                 if self.check_overlap(event.start_time, event.end_time, self.start_time, self.end_time):
-                    raise ValidationError('There is an overlap with another booking!: ' + str(event.day) + ' ' + str(event.start_time))
+                    raise ValidationError('There is an overlap with another booking!: ' + str(event.day)
+                                          + ' ' + str(event.start_time) + ' - ' + str(event.end_time))
--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from  .views import  get_booking_system
+from  .views import  get_index, get_booking_system
 urlpatterns = [
 
-    url(r'^$', get_booking_system)
+    url(r'^$', get_index, name='home'),
+    url(r'^new/booking/$', get_booking_system)
 ]
